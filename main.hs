bd148468aa702501b5c2edf319a83d43c23afa83
zoneWalkerG0X <33497380+zoneWalkerG0X@users.noreply.github.com>
2017-11-17 10:53:30 -0500

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,19 @@
+import Data.List.Split
+
+main :: IO()
+main = do
+ putStrLn "Zone's Abacus"
+ putStrLn "[operand] [operator] [operand] | +-*/%"
+ inpt <- getLine
+ let l = splitOn " " inpt
+ let a = read (l !! 0)::Float
+ let op = l !! 1
+ let b = read (l !! 2)::Float
+
+ case op of
+  "+" -> putStrLn(show (a + b))
+  "-" -> putStrLn(show (a - b))
+  "*" -> putStrLn(show (a * b))
+  "/" -> putStrLn(show (a / b))
+  "%" -> putStrLn(show ((floor a) `mod` (floor b)))
+  _ -> putStrLn "Unrecognized operator."	
